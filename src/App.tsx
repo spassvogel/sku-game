826f3cbed35295d98c2486e6360a8c64811593ae
Wouter van den Heuvel <exo_00@hotmail.com>
2020-05-16 22:06:56 +0200

    feat: a bit of space on top

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import './App.css';
 import Scene from 'components/Scene';
 import * as PIXI  from 'pixi.js';
+import { Stage } from '@inlet/react-pixi';
 window.PIXI = PIXI;
 
 function App() {
+  
+  const width = 672;
+  const height = 400;
+  const sceneHeight = 320;
+
   return (
     <div className="App">
-      <Scene tilemap="scenes/level1.json" />
+      <Stage width={width} height={height}>
+        <Scene tilemap="scenes/level1.json" width={width} height={height} y={height - sceneHeight}/>
+      </Stage>
     </div>
   );
 }
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -12,17 +12,20 @@ import 'pixi-tilemap'; // tilemap is not a real npm module :/
 
 export interface Props {
     tilemap: string;
+    width: number;
+    height: number;
 }
 
 const DEFAULT_WIDTH = 800;
 const DEFAULT_HEIGHT = 1000;
 
-const Scene = (props: Props) => {
+const Scene = (props: Props & React.ComponentProps<typeof Container>) => {
+    const {tilemap, width, height, ...restProps} = props;
     const [mapData, setMapData] = useState<TiledMapData>();
     const [blockedTiles, setBlockedTiles] = useState<number[][]>([]);
     const ref = useRef<PIXI.Container>(null);
 
-    const jsonPath = `${process.env.PUBLIC_URL}/${props.tilemap}`;
+    const jsonPath = `${process.env.PUBLIC_URL}/${tilemap}`;
 
     useEffect(() => {
         new PIXI.Loader().add(jsonPath).load((loader)=>{            
@@ -33,9 +36,6 @@ const Scene = (props: Props) => {
 
     const basePath = jsonPath.substr(0, jsonPath.lastIndexOf('/'));
 
-    const sceneWidth = (mapData?.width || 0) * (mapData?.tilewidth || 0) || DEFAULT_WIDTH;
-    const sceneHeight = (mapData?.height || 0) * (mapData?.tileheight || 0) || DEFAULT_HEIGHT;
-
     /** Returns true if the tile is blocked */
     const locationIsBlocked = useCallback((location: [number, number]) => {
         return blockedTiles.some((l) => l[0] === location[0] && l[1] === location[1]);
@@ -43,46 +43,45 @@ const Scene = (props: Props) => {
 
     return (
         <>
-            <Stage width={sceneWidth} height={sceneHeight}>
-                <Container 
-                    ref={ref}
-                    interactive={true} 
-                    hitArea={new PIXI.RoundedRectangle(0, 0, sceneWidth, sceneHeight, 0)}
-                >
-                    { mapData && (
-                        <Tilemap basePath={basePath} data={mapData} setBlockedTiles={setBlockedTiles}/>
-                    )}
-                    {/* { mapData && scene.actors.map((a) => (
-                        <SceneActor
-                            key={a.name}
-                            actor={a.name}
-                            questName={props.questName}
-                            tileWidth={mapData.tilewidth}
-                            tileHeight={mapData.tilewidth}
-                            location={a.location}
-                        >
-                            {selectedActor?.name === a.name && (<Graphics
-                                name="selectioncircle"
-                                draw={graphics => {
-                                    const line = 3;
-                                    graphics.lineStyle(line, 0xFFFFFF);
-                                    graphics.drawCircle(mapData.tilewidth / 2, mapData.tileheight / 2, mapData.tilewidth / 2 - line);
-                                    graphics.endFill();
-                                }}
-                            />)}
-                            <Sprite                     
-                                y={-80}
-                                image={`${process.env.PUBLIC_URL}/img/scene/actors/wizard.png`} 
-                                interactive={true}
-                                pointerdown={() => handleActorStartDrag(a)}
-                                pointerup={handleCancelAction}
-                                pointerupoutside={handleActorEndDrag}
-                            />
+            <Container 
+                ref={ref}
+                interactive={true}
+                hitArea={new PIXI.RoundedRectangle(0, 0, width, height, 0)}
+                {...restProps}
+            >
+                { mapData && (
+                    <Tilemap basePath={basePath} data={mapData} setBlockedTiles={setBlockedTiles}/>
+                )}
+                {/* { mapData && scene.actors.map((a) => (
+                    <SceneActor
+                        key={a.name}
+                        actor={a.name}
+                        questName={props.questName}
+                        tileWidth={mapData.tilewidth}
+                        tileHeight={mapData.tilewidth}
+                        location={a.location}
+                    >
+                        {selectedActor?.name === a.name && (<Graphics
+                            name="selectioncircle"
+                            draw={graphics => {
+                                const line = 3;
+                                graphics.lineStyle(line, 0xFFFFFF);
+                                graphics.drawCircle(mapData.tilewidth / 2, mapData.tileheight / 2, mapData.tilewidth / 2 - line);
+                                graphics.endFill();
+                            }}
+                        />)}
+                        <Sprite                     
+                            y={-80}
+                            image={`${process.env.PUBLIC_URL}/img/scene/actors/wizard.png`} 
+                            interactive={true}
+                            pointerdown={() => handleActorStartDrag(a)}
+                            pointerup={handleCancelAction}
+                            pointerupoutside={handleActorEndDrag}
+                        />
 
-                        </SceneActor>
-                    ))} */}
-                </Container>
-            </Stage>           
+                    </SceneActor>
+                ))} */}
+            </Container>
             {/* {DEBUG_ACTIONQUEUE && (
                 <div style={{ position: 'absolute', bottom: 0}}>
                     <h2>ActionQueue</h2>
