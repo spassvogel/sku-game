e77d7af0372c9b4edbe516a6d6f9dac2fc97e293
Wouter van den Heuvel <exo_00@hotmail.com>
2020-05-22 10:17:03 +0200

    Updates

---
+++ precache-manifest.3d46ab8d8bde6fe1edc1ab4a4b2b4aa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753819c1eddaa9312d94ab978ac61c84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d5e0d7dbc91a7935f78",
+    "url": "/static/css/main.9df01184.chunk.css"
+  },
+  {
+    "revision": "2c660ffa61be8f4a200d",
+    "url": "/static/js/2.0d040021.chunk.js"
+  },
+  {
+    "revision": "c2fb6df82dc1f4429fb8b222f47e654b",
+    "url": "/static/js/2.0d040021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5e0d7dbc91a7935f78",
+    "url": "/static/js/main.be743733.chunk.js"
+  },
+  {
+    "revision": "97a37751a24b999fc048",
+    "url": "/static/js/runtime-main.3bbf3c6d.js"
+  }
+]);
